f73d85e838cf1a94b7490a75e22d66068a1acca4
osamuraakinori <satoshihitoshiosamemura@satoshiuranoMBP.router.asus.com>
2016-11-02 13:36:05 +0900

    update README

--- cf.py
+++ cf.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from collections import defaultdict
 import numpy
+import pandas
 
 def jaccard(e1, e2):
 	sete1 = set(e1)
